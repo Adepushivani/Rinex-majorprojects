9b428c22deb2c55d4cfad26ea9b5d4b95f4c208c
Adepushivani <110152014+Adepushivani@users.noreply.github.com>
2022-09-06 13:22:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import joblib
+
+# load the joblib module
+model_nb=joblib.load("neg-pos")
+
+st.title("NEG POS CLASSIFIER")
+ip = st.text_input("Enter your text")
+
+op = model_nb.predict([ip])
+if st.button("PREDICT"):
+  st.title(op[0])
